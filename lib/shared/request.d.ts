e0d6899fb84b4fd9434fe87da64ed35363ba5e78
younghero <yxzhang@hitotek.com>
2022-04-14 16:55:34 +0800

    chore: del lib

--- lib/shared/request.d.ts
+++
@@ -1,4 +0,0 @@
-import type { AxiosInstance } from 'axios';
-export declare const axiosInstance: import("vue").Ref<AxiosInstance>;
-export declare function getRequest(): AxiosInstance;
-export declare function setRequest(ins: AxiosInstance): void;
