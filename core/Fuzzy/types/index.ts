68abfa957d2d73100ad58f18d7e1ebeb07d86082
younghero <yxzhang@hitotek.com>
2022-04-19 10:42:05 +0800

    chore: write js docx

--- core/Fuzzy/types/index.ts
+++ core/Fuzzy/types/index.ts
@@ -2,58 +2,146 @@ import type { Ref, VNode } from 'vue'
 import type { DialogStyle } from '../components/Dialog/types'
 
 interface TemplateConfiguration {
-  title?: string | { // 页面标题 可传递自定义图片
+  /**
+   * 页面标题 可传递自定义图片
+   */
+  title?: string | {
     text: string
     icon: string
   }
-  tabList?: Array<{ // 多页面启用
+  /**
+   * 多页面启用
+   */
+  tabList?: Array<{
     label: string
     value: any
   }>
-  dialogStyle?: DialogStyle | Array<DialogStyle> // 对话框样式
-  api: string | Api | Array<string> | Array<Api> // 增删改查接口地址
-  feature: Feature | Array<Feature> // 是否开启增删改查中其中一项功能
-  tableOperation?: tableOperation | Array<tableOperation> // 表格操作栏放置的自定义内容
-  pagination?: Pagination // 分页的配置 一页展示10条？
-  templates: Array<Templates> | Array<Array<Templates>> // 需要展示的每个字段 可配置每个字段对应的功能
-  isShownDialog?: Boolean | Array<Boolean> // 是否开启对话框
+  /**
+   * 对话框样式
+   */
+  dialogStyle?: DialogStyle | Array<DialogStyle>
+  /**
+   * 增删改查接口地址
+   */
+  api: string | Api | Array<string> | Array<Api>
+  /**
+   * 是否开启增删改查中其中一项功能
+   */
+  feature: Feature | Array<Feature>
+  /**
+   * 表格操作栏放置的自定义内容
+   */
+  tableOperation?: tableOperation | Array<tableOperation>
+  /**
+   * 分页的配置 一页展示10条？
+   */
+  pagination?: Pagination
+  /**
+   * 需要展示的每个字段 可配置每个字段对应的功能
+   */
+  templates: Array<Templates> | Array<Array<Templates>>
+  /**
+   * 是否开启对话框
+   */
+  isShownDialog?: Boolean | Array<Boolean>
 }
 
-// 是否开启增删改查中其中一项功能
+/**
+ * 是否开启增删改查中其中一项功能
+ */
 interface Feature {
   create?: boolean
   update?: boolean
   delete?: boolean
 }
 
-// 每个字段对应的相关信息
+/**
+ * 每个字段对应的相关信息
+ */
 interface Templates {
-  label?: string // 字段名称
-  value: string // 后端需要的字段值
-  type?: FormItemEnum // 该字段展示的表单类型 输入框？ 下拉框？ 时间选择器？
-  datePickerType?: string // 时间选择器类型 对照element-plus的时间选择器类型
+  /**
+   *  字段名称
+   */
+  label?: string
+  /**
+   *  后端需要的字段值
+   */
+  value: string
+  /**
+   * 该字段展示的表单类型 输入框？ 下拉框？ 时间选择器？
+   */
+  type?: FormItemEnum
+  /**
+   * 时间选择器类型 对照element-plus的时间选择器类型
+   */
+  datePickerType?: string
+  /**
+   * 时间选择器选择范围分割符
+   */
   rangeSeparator?: string
+  /**
+   * 时间段选择器右边占位符
+   */
   startPlaceholder?: string
+  /**
+   * 时间段选择器右边占位符
+   */
   endPlaceholder?: string
-  defaultQueryValue?: string // 表单展示的默认值
-  rules?: [] // 该字段的表单规则 参照element-plus
-  require?: boolean // 是否必填
+  /**
+   * 查询区域该字段展示的默认值
+   */
+  defaultQueryValue?: string
+  /**
+   *  该字段的表单规则 参照element-plus
+   */
+  rules?: []
+  /**
+   * 是否必填
+   */
+  require?: boolean
+  /**
+   * 占位符
+   */
   placeholder?: string
-  items?: Array<{ // type 为下拉框时的options
+  /**
+   * type 为下拉框时的options
+   */
+  items?: Array<{
     label: {} | string
     value: string | number
     type?: number | string
   }>
+  /**
+   * 在表格中多宽
+   */
   width?: number | string
-  visible?: { // 是否在增删改查中包含此字段
+  /**
+   * 是否在增删改查中包含此字段
+   *
+   */
+  visible?: {
     query?: boolean
     table?: boolean
     create?: boolean
     update?: boolean
   }
-  fetchQuery?: (temp: Templates) => any // 异步加载下拉框的options
-  render?(param: any): any // 在表格中如何展示该字段的样子
+  /**
+   * 异步加载下拉框的options
+   */
+  fetchQuery?: (temp: Templates) => any
+  /**
+   * 在表格中自定义展示该字段
+   * 按钮的形式？
+   * 给字段带点颜色？
+   */
+  render?(param: any): any
 }
+
+/**
+ * 表格操作区自定义
+ * 不需要编辑？ 删除？
+ * 需要下载？
+ */
 interface tableOperation {
   show?: boolean
   operator?: Array<{
@@ -85,7 +173,9 @@ interface AllModels {
   pagingModel: any
 }
 
-// 表单类型
+/**
+ * 表单类型
+ */
 enum FormItemEnum {
   select = 1,
   input = 2,
@@ -105,27 +195,61 @@ interface HandlerParams {
   current: any
 }
 
-// 提供Fuzzy自动处理增删改查时的生命周期
-// 查询时与要添加业务无关的字段？
-// 更新时要再次修改字段的value？
-// 删除时要多带一些数据给后端？
+/**
+ * 提供Fuzzy自动处理增删改查时的生命周期
+ * 查询时与要添加业务无关的字段？
+ * 更新时要再次修改字段的value？
+ * 删除时要多带一些数据给后端？
+ */
 interface FuzzyHandler {
-  queryBefore?: (params: HandlerParams) => any // 查询前
-  createBeforePop?: (params: HandlerParams) => any // 点击更新对话框出来前
-  updateBeforePop?: (params: HandlerParams) => any // 点击编辑对话框出来前
-  deleteBefore?: (params: HandlerParams) => any // 删除前
-  createConfirm?: (params: HandlerParams) => Promise<any> // 确认更新时
-  updateConfirm?: (params: HandlerParams) => Promise<any> // 新增更新时
-  createCancel?: (params: HandlerParams) => any // 取消更新时
-  updateCancel?: (params: HandlerParams) => any // 取消编辑时
+  /**
+   * 查询前
+   */
+  queryBefore?: (params: HandlerParams) => any //
+  /**
+   * 点击更新对话框出来前
+   */
+  createBeforePop?: (params: HandlerParams) => any //
+  /**
+   * 点击编辑对话框出来前
+   */
+  updateBeforePop?: (params: HandlerParams) => any //
+  /**
+   * 删除前
+   */
+  deleteBefore?: (params: HandlerParams) => any //
+  /**
+   * 确认更新时
+   */
+  createConfirm?: (params: HandlerParams) => Promise<any> //
+  /**
+   * 新增更新时
+   */
+  updateConfirm?: (params: HandlerParams) => Promise<any> //
+  /**
+   * 取消更新时
+   */
+  createCancel?: (params: HandlerParams) => any //
+  /**
+   * 取消编辑时
+   */
+  updateCancel?: (params: HandlerParams) => any
 }
 
-// 因公司项目新增表单、编辑表单比较复杂 暂未实现自动更新、编辑
-// 选择让用户自己提供编辑或新增时的组件
-// 点击确定、取消事件 会触发FuzzyHandler.createConfirm ....
+/**
+ * 因公司项目新增表单、编辑表单比较复杂 暂未实现自动更新、编辑
+ * 选择让用户自己提供编辑或新增时的组件
+ * 点击确定、取消事件 会触发FuzzyHandler.createConfirm ....
+ */
 interface RenderDialog {
-  UpdateRender?: VNode // 编辑对话框内展示的内容
-  CreateRender?: VNode // 更新对话框内展示的内容
+  /**
+   * 编辑对话框内展示的内容
+   */
+  UpdateRender?: VNode
+  /**
+   * 更新对话框内展示的内容
+   */
+  CreateRender?: VNode
 }
 interface DialogChanageable {
   text: string
