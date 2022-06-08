a1a807ca772c09a0672ee000409e7f66bffc418f
younghero <yxzhang@hitotek.com>
2022-06-08 15:59:07 +0800

    feat: expose querymode

--- playground/App.vue
+++ playground/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import Fuzzy from '../core/Fuzzy/index.vue'
+import type { FuzzyHandler } from '../core/Fuzzy/types'
 import { config } from './assets/config'
+
+const handler: FuzzyHandler = {
+  queryBefore: async() => {
+    console.log(11)
+  },
+}
 </script>
 
 <template>
   <div class="w-full h-full box-border bg-light-100">
-    <Fuzzy :config="config" />
+    <Fuzzy :config="config" :handler="handler" />
   </div>
 </template>
 <style>
